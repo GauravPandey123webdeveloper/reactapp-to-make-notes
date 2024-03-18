592b0bacbf365145b6b827b73c60cb98b991062a
keshav2025 <keshavgupta86036@gmail.com>
2024-03-18 11:12:41 +0530

    Notebook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes,Route} from 'react-router-dom'
+'
 
 import Sidebar from './components/Sidebar';
 import Cards from './components/Cards';
@@ -6,12 +6,7 @@ import Cards from './components/Cards';
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Sidebar/>}/>
-        <Route path='/Cards' element={<Cards/>}/>
-       
-        
-      </Routes>
+      <Sidebar/>
       <Cards/>
     </div>
   );
