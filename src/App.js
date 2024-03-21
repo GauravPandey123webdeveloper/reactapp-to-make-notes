42c3ae8ae63981444ca6b2ff8d3e00cdc37e8a99
Mohit Chauhan <chauhanmohit@gmail.com>
2024-03-21 21:36:11 +0530

    my change

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/notes' element={<Notes/>}/>
-        <Route path='/nav' element={<Navbar/>}/>
-      </Routes>
+      
     </div>
   );
 }
